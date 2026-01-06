cb5d7e4c899f5181b5dd0924a96e77a026e61cf0
Aditya <adityanegi804@gmail.com>
2026-01-06 17:57:49 +0530

    Solved 2 Patterns

---
+++ variable-sized-window.cpp
@@ -0,0 +1,210 @@
+
+// Problem 1 -  Minimum Size Subarray Sum
+
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+// Example 1:
+
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+// Example 2:
+
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+// Example 3:
+
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= target <= 109
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+ 
+// Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+
+// Approach 1 - Brute Force (Time Complexity-O(n^2))
+
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int n = nums.size();
+
+//         int len = INT_MAX;
+
+//         for(int i=0;i<n;i++){
+
+//             int sum = 0;
+//             for(int j=i;j<n;j++){
+
+//                 sum = sum + nums[j];
+//                 if(sum >= target){
+//                     len = min(len,j-i+1);
+//                     break;//our task was of minimum length
+//                 }
+//             }
+//         }
+
+//         return (len == INT_MAX)?0:len;
+//     }
+// };
+
+
+//Approach 2 - Time complexity of O(n)
+
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int n = nums.size();
+
+//         int len = INT_MAX;
+
+//         int l = 0,r = 0;
+//         int sum = 0;
+
+//         while(r < n){
+
+//             sum = sum + nums[r];
+
+//             while(l <= r && sum >= target){
+//                 len = min(len,r-l+1);
+//                 sum -= nums[l];
+//                 l++;
+//             }
+
+//             r++;
+//         }
+        
+//         return (len == INT_MAX)?0:len;
+//     }
+// };
+
+
+
+//Approach 3 - Time complexity of O(nlogn)
+
+// class Solution {
+// public:
+
+//     int binarySearch(vector<int>& prefix,int low,int high,int target){
+
+//         int sumToExclude = (low == 0)?0:prefix[low-1];
+//         int ans = -1;
+//         while(low <= high){
+
+//             int mid = low + floor((high-low)/2);
+
+//             if(prefix[mid] - sumToExclude >= target){
+//                 ans = mid;
+//                 high = mid-1;
+//             }
+//             else{
+//                 low = mid+1;
+//             }
+//         }
+
+//         return ans;
+//     }
+//     int minSubArrayLen(int target, vector<int>& nums) {
+        
+//         int n = nums.size();
+//         int len = INT_MAX;
+
+//         vector<int> prefix = nums;
+
+//         for(int i=1;i<n;i++){
+
+//             prefix[i] += prefix[i-1];
+//         }
+
+//         for(int i=0;i<n;i++){
+
+//             int j = binarySearch(prefix,i,n-1,target);
+
+//             if(j == -1) continue;
+
+//             len = min(len, j-i+1);
+//         }
+
+//         return (len == INT_MAX)?0:len;
+//     }
+// };
+
+
+// -------------------------------------------------------------------------------------------------------------
+
+// Problem 2 - Number of Subarrays with Bounded Maximum
+
+// Given an integer array nums and two integers left and right, return the number of contiguous non-empty subarrays such that the value of the maximum array element in that subarray is in the range [left, right].
+// The test cases are generated so that the answer will fit in a 32-bit integer.
+
+// Example 1:
+
+// Input: nums = [2,1,4,3], left = 2, right = 3
+// Output: 3
+// Explanation: There are three subarrays that meet the requirements: [2], [2, 1], [3].
+// Example 2:
+
+// Input: nums = [2,9,2,5,6], left = 2, right = 8
+// Output: 7
+ 
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 0 <= nums[i] <= 109
+// 0 <= left <= right <= 109
+
+// class Solution {
+// public:
+//     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        
+//         int n= nums.size();
+//         int target = left-1;
+
+//         //firstly count those subarrays whose ,maxiomum element is at maximum equal to maxi
+
+//         int i = 0,j = 0;
+//         int maxi = INT_MIN;
+//         int tillLeftCnt= 0;
+
+//         while(j < n){
+
+//             maxi = max(maxi,nums[j]);
+
+//             if(maxi > target){
+//                 int len = j-i;
+//                 maxi = INT_MIN;
+//                 tillLeftCnt += ((long long)len*(len+1))/2; 
+//                 i = j+1;
+//             }
+//             j++;
+//         }
+
+//         int len = j-i;
+//         tillLeftCnt += ((long long)len*(len+1))/2; 
+
+//         target = right;
+//         i = 0,j = 0,maxi = INT_MIN;
+//         int tillRightCnt = 0;
+//         while(j < n){
+
+//             maxi = max(maxi,nums[j]);
+
+//             if(maxi > target){
+//                 int len = j-i;
+//                 maxi = INT_MIN;
+//                 tillRightCnt += ((long long)len*(len+1))/2; 
+//                 i = j+1;
+//             }
+//             j++;
+//         }
+
+//         len = j-i;
+//         tillRightCnt += ((long long)len*(len+1))/2;
+
+//         return tillRightCnt - tillLeftCnt;
+//     }
+// };

aeb452072c9c08b9e08b09958ed3d7bef9cc374e
Aditya <adityanegi804@gmail.com>
2026-01-09 16:20:23 +0530

    solved other two patterns

--- variable-sized-window.cpp
+++ variable-sized-window.cpp
@@ -568,3 +568,132 @@
 //         return res;
 //     }
 // };
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// Problem 7 - Count the Number of Good Subarrays
+
+// Given an integer array nums and an integer k, return the number of good subarrays of nums.
+// A subarray arr is good if there are at least k pairs of indices (i, j) such that i < j and arr[i] == arr[j].
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1:
+
+// Input: nums = [1,1,1,1,1], k = 10
+// Output: 1
+// Explanation: The only good subarray is the array nums itself.
+// Example 2:
+
+// Input: nums = [3,1,4,3,2,2,4], k = 2
+// Output: 4
+// Explanation: There are 4 different good subarrays:
+// - [3,1,4,3,2,2] that has 2 pairs.
+// - [3,1,4,3,2,2,4] that has 3 pairs.
+// - [1,4,3,2,2,4] that has 2 pairs.
+// - [4,3,2,2,4] that has 2 pairs.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i], k <= 109
+
+// class Solution {
+// public:
+//     long long countGood(vector<int>& nums, int k) {
+        
+//         long long res = 0;
+
+//         int l = 0,r = 0;
+
+//         int n= nums.size();
+        
+//         unordered_map<int,int> mpp;
+
+//         long long cnt = 0;
+//         while(r < n){
+
+//             cnt = cnt + mpp[nums[r]];
+//             mpp[nums[r]]++;
+
+//             while(l <= r && cnt >= k){
+//                 mpp[nums[l]]--;
+//                 res = res + (n-r);
+//                 if(mpp[nums[l]] == 0) mpp.erase(nums[l]);
+//                 cnt = cnt - mpp[nums[l]];
+//                 l++;
+//             }
+//             r++;
+//         }
+//         return res;
+//     }
+
+// };
+
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// Problem 8 - Shortest Subarray With OR at Least K II
+
+// You are given an array nums of non-negative integers and an integer k.
+// An array is called special if the bitwise OR of all of its elements is at least k.
+// Return the length of the shortest special non-empty subarray of nums, or return -1 if no special subarray exists.
+
+// Example 1:
+
+// Input: nums = [1,2,3], k = 2
+// Output: 1
+// Explanation:
+// The subarray [3] has OR value of 3. Hence, we return 1.
+
+// Example 2:
+
+// Input: nums = [2,1,8], k = 10
+// Output: 3
+// Explanation:
+// The subarray [2,1,8] has OR value of 11. Hence, we return 3.
+
+// Example 3:
+
+// Input: nums = [1,2], k = 0
+// Output: 1
+// Explanation:
+// The subarray [1] has OR value of 1. Hence, we return 1.
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 105
+// 0 <= nums[i] <= 109
+// 0 <= k <= 109
+
+// class Solution {
+// public:
+//     long long countGood(vector<int>& nums, int k) {
+        
+//         long long res = 0;
+
+//         int l = 0,r = 0;
+
+//         int n= nums.size();
+        
+//         unordered_map<int,int> mpp;
+
+//         long long cnt = 0;
+//         while(r < n){
+
+//             cnt = cnt + mpp[nums[r]];
+//             mpp[nums[r]]++;
+
+//             while(l <= r && cnt >= k){
+//                 mpp[nums[l]]--;
+//                 res = res + (n-r);
+//                 if(mpp[nums[l]] == 0) mpp.erase(nums[l]);
+//                 cnt = cnt - mpp[nums[l]];
+//                 l++;
+//             }
+//             r++;
+//         }
+//         return res;
+//     }
+
+// };
